c42641e48220319c0583af8aa235c9c9dcfd4a81
doria <doria@192.168.115.155>
2021-10-04 12:37:49 +0200

    creation du projet tp1.1

--- Manipnombresint/src/manipnombresint/Manipnombresint.java
+++ Manipnombresint/src/manipnombresint/Manipnombresint.java
@@ -4,6 +4,8 @@
  */
 package manipnombresint;
 
+import java.util.Scanner;
+
 /**
  *
  * @author doria
@@ -14,7 +16,30 @@ public class Manipnombresint {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanneurClav;
+        scanneurClav = new Scanner(System.in);
+        int somme = 0;
+        int difference = 0;
+        int produit = 0;
+        int quotint = 0;
+        int reste = 0;
+       
+        System.out.println("Entrez le nombre 1");
+        int nombre1 = scanneurClav.nextInt();
+        System.out.println("Entrez le nombre 2");
+        int nombre2 = scanneurClav.nextInt();
+        
+        somme = nombre1+nombre2;
+        difference = nombre1-nombre2;
+        produit = nombre1*nombre2;
+        quotint = nombre1/nombre2;
+        reste = nombre1%nombre2;
+        
+        System.out.println("La somme des deux nombres est: "+somme);
+        System.out.println("La différence entre ces deux nombres est: "+difference);
+        System.out.println("Le produit des deux nombres est: "+produit);
+        System.out.println("Le quotient entier des deux nombres est: "+quotint);
+        System.out.println("Le reste de la division euclidienne deux nombres est: "+reste);
     }
     
 }
