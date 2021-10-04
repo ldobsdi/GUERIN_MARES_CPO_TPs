9108052914ed163cc48b7ddd5760855c30fbdcef
doria <doria@192.168.115.155>
2021-10-04 12:43:31 +0200

    creation du projet tp1.3

--- GuessMyNumber/src/guessmynumber/GuessMyNumber.java
+++ GuessMyNumber/src/guessmynumber/GuessMyNumber.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package javaapplication9;
+package guessmynumber;
 
 /**
  *
  * @author doria
  */
-public class JavaApplication9 {
+public class GuessMyNumber {
 
     /**
      * @param args the command line arguments
