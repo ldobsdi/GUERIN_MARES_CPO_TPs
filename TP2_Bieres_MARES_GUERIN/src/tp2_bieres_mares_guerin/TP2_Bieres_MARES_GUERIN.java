675ed5ccfcdfc11ec87cf9fc1476ce5f002eed9b
egnma <egnma@192.168.115.184>
2021-10-08 11:17:47 +0200

    Version vraiment pfinale

--- TP2_Bieres_MARES_GUERIN/src/tp2_bieres_mares_guerin/TP2_Bieres_MARES_GUERIN.java
+++ TP2_Bieres_MARES_GUERIN/src/tp2_bieres_mares_guerin/TP2_Bieres_MARES_GUERIN.java
@@ -14,35 +14,45 @@ public class TP2_Bieres_MARES_GUERIN {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       BouteilleBiere Biere1 = new BouteilleBiere("Cuvéee des trolls ", (double) 7.0, "Dubuisson") ;
-       /*Biere1.Nom = "Cuvée des trolls";
-       Biere1.degreAlcool = (double)7.0 ;
-       Biere1.brasserie = "Dubuisson";*/
-       Biere1.ouverte = false ;
-       Biere1.lireEtiquette();
-       System.out.println("La bouteille est ouverte ? : "+Biere1.ouverte);
+       BouteilleBiere UneBiere = new BouteilleBiere("Cuvéee des trolls ", (float) 7.0, "Dubuisson") ;
+       UneBiere.Nom = "Cuvée des trolls";
+       UneBiere.degreAlcool = (float)7.0 ;
+       UneBiere.brasserie = "Dubuisson";
+       UneBiere.ouverte = false ;
+       UneBiere.lireEtiquette();
+       System.out.println("La bouteille est ouverte ? : "+UneBiere.ouverte);
        //Ouverture de la bière
        System.out.println("Je décapsule la bière");
-       Biere1.Décapsuler();
-       System.out.println(Biere1) ;
+       UneBiere.Décapsuler();
+       System.out.println(UneBiere) ;
        
-       BouteilleBiere Biere2 = new BouteilleBiere("Leffe", (float) 6.6, "Abbaye de Leffe") ;
-       /*Biere2.Nom = "Leffe";
-       Biere2.degreAlcool = (float)6.6 ;
-       Biere2.brasserie = "Abbaye de Leffe";*/
-       Biere2.lireEtiquette();
-       Biere2.ouverte = true ;
-       Biere2.Décapsuler();
-       System.out.println(Biere2) ;
+       BouteilleBiere autreBiere = new BouteilleBiere("Leffe", (float) 6.6, "Abbaye de Leffe") ;
+       autreBiere.Nom = "Leffe";
+       autreBiere.degreAlcool = (float)6.6 ;
+       autreBiere.brasserie = "Abbaye de Leffe";
+       autreBiere.lireEtiquette();
+       autreBiere.ouverte = true ;
+       autreBiere.Décapsuler();
+       System.out.println(autreBiere) ;
        
-       BouteilleBiere Biere3 = new BouteilleBiere("Pelfort", (float) 7.6, "Fort de Pelfort") ;
-       Biere3.lireEtiquette();
-       BouteilleBiere Biere4 = new BouteilleBiere("Ikki", (float) 4.5, "Japan") ;
-       Biere4.lireEtiquette();
-       BouteilleBiere Biere5 = new BouteilleBiere("Saitama", (float) 100, "Ville Z") ;
-       Biere5.lireEtiquette();
+       BouteilleBiere BierePelforth = new BouteilleBiere("Pelforth", (float) 7.6, "Brassée dans le Nord") ;
+       BierePelforth.ouverte = true ;
+       BierePelforth.lireEtiquette();
+       BierePelforth.Décapsuler();
+       System.out.println(BierePelforth) ;
+       
+       BouteilleBiere BierePietra = new BouteilleBiere("Pietra", (float) 6, "Brasserie Pietra") ;
+       BierePietra.ouverte = false ;
+       BierePietra.lireEtiquette();
+       BierePietra.Décapsuler();
+       System.out.println(BierePietra) ;
+       
+       BouteilleBiere BiereBelleGueule = new BouteilleBiere("Belle gueule", (float) 5.2, "Brasseur Ergi") ;
+       BiereBelleGueule.ouverte = true ;
+       BiereBelleGueule.Décapsuler();
+       BiereBelleGueule.lireEtiquette();
+       System.out.println(BiereBelleGueule) ;
     }
 
     }
     
-}
