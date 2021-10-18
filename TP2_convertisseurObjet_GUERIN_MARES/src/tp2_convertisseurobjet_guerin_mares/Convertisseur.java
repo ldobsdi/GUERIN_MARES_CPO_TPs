5399e124296728c138f3cc0f9a70b1a39f70281c
doria <doria@192.168.115.155>
2021-10-18 16:21:42 +0200

    création convertisseur

---
+++ TP2_convertisseurObjet_GUERIN_MARES/src/tp2_convertisseurobjet_guerin_mares/Convertisseur.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_convertisseurobjet_guerin_mares;
+
+
+
+/**
+ *
+ * @author doria
+ */
+
+public class Convertisseur {
+    
+    double tempCelcius;
+    double tempKelvin ;
+    double conversion ;
+    double t;
+    double result;
+    int nbConversions;
+    public Convertisseur () {
+       nbConversions = 0;  
+    }
+    
+    
+    public double CelciusVersKelvin (double t) { 
+      t = t + 273.15;
+      System.out.println("température en Kelvin: " + t);
+      nbConversions += 1;
+      return t;
+      
+     }
+    
+    public double KelvinVersCelsius ( double t) { 
+     t = t - 273.15;
+     System.out.println("température en Celcius: " + t);
+     nbConversions += 1;
+     return (float) t;
+     
+    }
+    
+    public double CelciusVersFarenheit ( double t) { 
+     t = t * 1.8000 + 32.00;
+     System.out.println("température en Farenheit: " + t);
+     nbConversions += 1;
+     System.out.println(nbConversions);
+     return t;
+     
+    }
+    
+    public double KelvinVersFarenheit ( double t) { 
+     t = (t * 9.0/5.0)-459.67;
+     System.out.println("température en Farenheit: " + t);
+     nbConversions += 1;
+     System.out.println(nbConversions);
+     return (double) t;
+    }
+    
+    public double FarenheitVersCelsius ( double t) { 
+     
+     t = (t-32)/1.800000;
+     System.out.println("température en Celcius: " + t);
+     nbConversions += 1;
+     System.out.println(nbConversions);
+      return 0;
+      
+    }
+    
+    public double FarenheitVersKelvin ( double t) { 
+     t= (t-32)/1.8 + 273.15;
+     System.out.println("température en Kelvin: " + t);
+     nbConversions += 1;
+     System.out.println("Le nombre de conversions est:" + nbConversions);
+     return (float) (t);
+    }
+    @Override
+    public String toString () {
+        return ("nb de conversions"+ nbConversions);
+     
+}
+    
+    
+}
+
+
---
+++ TP2_convertisseurObjet_GUERIN_MARES/src/tp2_convertisseurobjet_guerin_mares/TP2_convertisseurObjet_GUERIN_MARES.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_convertisseurobjet_guerin_mares;
+
+/**
+ *
+ * @author doria
+ */
+public class TP2_convertisseurObjet_GUERIN_MARES {
+
+    private static boolean nbConversions;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Convertisseur conversions = new Convertisseur();
+        conversions.CelciusVersFarenheit(0);
+        conversions.FarenheitVersCelsius(43);
+        
+}
+}
