4609cd82f55e44abd6c5a134c58d528ad84e4460
egnma <egnma@192.168.115.184>
2021-10-25 12:08:00 +0200

    SUPER PUISSANT 4 : interface graphique

---
+++ SP4_graphique_GUERRRRIN _MARES/src/sp4_graphique_guerrrrin/_mares/SP4_graphique_GUERRRRIN_MARES.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sp4_graphique_guerrrrin._mares;
+
+/**
+ *
+ * @author egnma
+ */
+public class SP4_graphique_GUERRRRIN_MARES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
