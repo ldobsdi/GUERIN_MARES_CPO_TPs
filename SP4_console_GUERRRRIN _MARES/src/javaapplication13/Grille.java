adfaa62f61620d116177d32f40ac27ded0ca3e26
doria <doria@192.168.114.128>
2021-11-12 16:39:59 +0100

    classes grille, joueur, jeton et methodes

--- SP4_console_GUERRRRIN _MARES/src/javaapplication13/Grille.java
+++ SP4_console_GUERRRRIN _MARES/src/javaapplication13/Grille.java
@@ -13,15 +13,22 @@ public class Grille {
     Cellule [][] CellulesJeu = new Cellule[6][7];
     
     public void Grille() {
-
-    
-for (int i = 0; i < 6 ; i++) {
-    for (int j = 0; j < 7; j++){
+        for (int i = 0; i < 6 ; i++) {
+            for (int j = 0; j < 7; j++){
         CellulesJeu [i][j] = null;
     }
 }
+}
+}
 
+public boolean ajouterJetonDansColonne(Jeton, int){
+}
+}
+    
+public boolean etreRemplie () {
+boolean etreRemplie = true;
 
+}
     
 
 
