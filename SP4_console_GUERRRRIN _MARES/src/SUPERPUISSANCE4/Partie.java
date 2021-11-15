00c499736ddc48e942c2cc0456ef4da93fd14a62
egnma <egnma@192.168.115.184>
2021-11-15 16:45:31 +0100

    CA AVANCE ...

--- SP4_console_GUERRRRIN _MARES/src/SUPERPUISSANCE4/Partie.java
+++ SP4_console_GUERRRRIN _MARES/src/SUPERPUISSANCE4/Partie.java
@@ -36,66 +36,6 @@ public class Partie {
     }
     
     public void initialiserPartie(){
-        
-        //Création ou réinitialisation de la grille de jeu
-        
-        //Création de la grille de jeu
-        if (this.GrilleDeJeu == null){
-            GrilleDeJeu = new Grille();
-        }
-        
-        //Réinitialisation de la grille de jeu
-        else{
-            GrilleDeJeu.viderGrille();
-        }
-        
-        //placement des trou noirs
-        int compteurtn = 0; //permet de parcourir la boucle while liée aux trous noirs
-        
-        int compteurdses = 0;// Ce compteur permet de compter le nombre de désintégrateurs
-                         //placés sur des trou noirs
-        int compteurDes = 0;//permet de parcourir la boucle while liée aux désintégrateurs
-        Random rand = new Random();
-        int coor1;
-        int coor2;
-        while (compteurtn != 5){
-            //Coordonnées aléatoires
-            coor1 = rand.nextInt(6);
-            coor2 = rand.nextInt(7);
-            
-            if (GrilleDeJeu.Cellules[coor1][coor2].placerTrouNoir()){
-                //Si le placement du trou noir est possible, on rajoute un désintégrateur.
-                if (compteurdses < 2){
-                GrilleDeJeu.Cellules[coor1][coor2].placerDesintegrateur();
-                compteurdses++;//On incrément le compteur trounoir/Désintégrateur dans la boucle
-                }
-                
-                compteurtn+=1;//On incrémente le compteur trou noir dans la boucle
-            }
-        }
-        //Une fois les 5 trou noirs et les 2 désintégrateurs associés placés, il reste à placer les 3 désintégrateurs restants
-        
-        while (compteurDes<3){
-            coor1 = rand.nextInt(6);
-            coor2 = rand.nextInt(7);
-            
-            //On place les trois désintégrateurs restants sur des cases où il n'y a pas de trou noir et de desintegrateur
-            if (!GrilleDeJeu.Cellules[coor1][coor2].presenceTrouNoir() && 
-                !GrilleDeJeu.Cellules[coor1][coor2].presenceDesintegrateur()){
-                GrilleDeJeu.Cellules[coor1][coor2].placerDesintegrateur();
-                compteurDes++;
-            }
-        }
-        
-        //attribution des jetons
-        for (int i = 0; i<21; i++){
-            //rajoute un jeton
-            
-            ListeJoueurs[0].ajouterJeton(new Jeton(ListeJoueurs[0].Couleur));
-            ListeJoueurs[1].ajouterJeton(new Jeton(ListeJoueurs[1].Couleur));
-        }
-        
-        
     }
     
     public void debuterPartie() {
