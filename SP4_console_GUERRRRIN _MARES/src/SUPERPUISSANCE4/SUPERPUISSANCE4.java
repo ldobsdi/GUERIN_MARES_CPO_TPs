b07cb578ed517f7d7b6368820c495a717a4b1b9b
egnma <egnma@192.168.115.184>
2021-11-26 13:00:07 +0100

    Terminé, maintenant il faut corriger les erreurs de lancement

--- SP4_console_GUERRRRIN _MARES/src/SUPERPUISSANCE4/SUPERPUISSANCE4.java
+++ SP4_console_GUERRRRIN _MARES/src/SUPERPUISSANCE4/SUPERPUISSANCE4.java
@@ -1,8 +1,10 @@
+package SUPERPUISSANCE4;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package SUPERPUISSANCE4;
+
 import java.util.Scanner;
 
 /**
